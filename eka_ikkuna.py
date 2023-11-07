e6d965f486106aeb5d951fa411937d64a959739a
Kriisi <Kriisi@users.noreply.github.com>
2023-11-07 23:09:37 +0200

    Add files via upload

--- eka_ikkuna.py
+++ eka_ikkuna.py
@@ -5,19 +5,19 @@ import pygame
 pygame.init()
 
 # luodaan taustaikkuna, canvas
-tausta = pygame.display.set_mode((800, 420))
+tausta = pygame.display.set_mode((1280, 720))
 
 # kirjoitetaan otsikkopalkkiin
 pygame.display.set_caption("Hoplaa!") 
 
 # 3. ladataan kuvatiedosto
-kuva = pygame.image.load("joku_kuva.png") 
+# kuva = pygame.image.load("joku_kuva.png") 
 
 # 3. asetetaan kuvalle piirtopaikka
-yla_oikea = (60,120)
+# yla_oikea = (0,0)
 
 # 2. annetaan neliölle väri 
-nelio_vari = (255,255,0)
+# nelio_vari = (255,0,0)
 
 # asetetaan pääsilmukan ehtomuuttuja todeksi, että pysytään pelissä
 running = True
@@ -33,10 +33,10 @@ while running:
     tausta.fill("purple")
 
     # 3. blit() metodilla kuva näytölle haluttuun paikkaan
-    tausta.blit(kuva, dest = yla_oikea) 
+    # tausta.blit(kuva, dest = yla_oikea) 
 
     # 2. piirretään neliö omalla värillään
-    pygame.draw.rect(tausta, nelio_vari, pygame.Rect(30,30,60,60)) 
+    # pygame.draw.rect(tausta, nelio_vari, pygame.Rect(30,30,60,60)) 
 
     # päivitetään muutokset näytölle
     pygame.display.update() 
